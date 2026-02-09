46cf2eba677cb6c498513726364b1c3793bea0d8
avgra3 <antonygradillas@gmail.com>
2026-02-09 09:52:46 -0700

    Updated recipe build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,10 +8,10 @@ export MARIADB_CC_LIBRARY="${PREFIX}/include/mariadb"
 export PATH="$MARIADB_CC_LIB:$MARIADB_CC_LIBRARY:${PATH}"
 export MARIADB_CONFIG="${PREFIX}/bin/mariadb_config"
 
-# if [[ "${target_platform}" == "osx-arm64" ]]; then
-# 	export CXXFLAGS="$CXXFLAGS --cc -arch arm64"
-# 	export CFLAGS="$CFLAGS -arch arm64"
-# fi
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+	export CXXFLAGS="$CXXFLAGS --cc -arch arm64"
+	export CFLAGS="$CFLAGS -arch arm64"
+fi
 
 ${PYTHON} setup.py build
 ${PYTHON} -m pip install . -vvv --no-deps --no-build-isolation --no-cache-dir
