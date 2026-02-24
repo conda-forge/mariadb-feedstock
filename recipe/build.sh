728bb58b070de864c76ad0e0069e2688673b1a26
avgra3 <antonygradillas@gmail.com>
2026-02-24 16:10:16 -0700

    change to posix recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,9 +8,6 @@ export MARIADB_CC_LIBRARY="${PREFIX}/include/mariadb"
 export PATH="$MARIADB_CC_LIB:$MARIADB_CC_LIBRARY:${PATH}"
 export MARIADB_CONFIG="${PREFIX}/bin/mariadb_config"
 
-if [[ "$TARGET_PLATFORM" == "osx-arm64" ]]; then
-	export CC="$CC:arm64-apple-darwin20.0.0-clang"
-fi
 
 ${PYTHON} setup.py build
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
+${PYTHON} -m pip install . -vv # --no-deps --no-build-isolation
